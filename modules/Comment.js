41da7dcd71aaf26a21ccee5f679957eb225e214f
Dmitry Mekong <kenig.dmitry@gmail.com>
2023-05-11 18:51:14 -0400

    some code

---
+++ modules/Comment.js
@@ -0,0 +1,15 @@
+class Comment{
+    constructor(text){
+        this.text = text
+        this.votesQty = 0
+    }
+    
+    upvote(){
+        this.votesQty += 1
+    }
+}
+
+const firstComment = new Comment("First comment")
+firstComment.upvote()
+
+
